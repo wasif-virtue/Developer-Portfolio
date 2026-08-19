5bdec8b28806efe54c1a8aa0caf73a8de7e1d2b1
Muhammad Wasif Raza <virtuevisioncompany@gmail.com>
2026-08-19 13:33:49 -0700

    Experience Section completed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
  const resumeBtns = document.querySelectorAll('.resume-btn');
  resumeBtns.forEach((btn, idx) => {
      btn.addEventListener('click', () => {
+        const resumeDetails = document.querySelectorAll('.resume-detail');
+        resumeDetails.forEach(d => d.classList.remove('active'));
+        resumeDetails[idx].classList.add('active');
          resumeBtns.forEach(b => b.classList.remove('active'));
          btn.classList.add('active');
      });
