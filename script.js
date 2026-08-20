4de8e09a0bf5b80db1cd9b5ff98bcc2587745058
Muhammad Wasif Raza <virtuevisioncompany@gmail.com>
2026-08-20 15:08:15 -0700

    All Sections completed

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
- const resumeBtns = document.querySelectorAll('.resume-btn');
+const navLinks = document.querySelectorAll('header nav a');
+const logoLinks = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+
+const activePage = () => {
+    const barsBox = document.querySelector('.bars-box');
+    const header = document.querySelector('header');
+
+    header.classList.remove('active');
+
+    setTimeout(() => {
+        header.classList.add('active');
+    }, 1100);
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    })
+
+    barsBox.classList.remove('active');
+
+    setTimeout(() => {
+        barsBox.classList.add('active');
+
+    }, 1100);
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    })
+}
+
+navLinks.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if (!link.classList.contains('active')) {
+            activePage();
+            link.classList.add('active');
+            setTimeout(() => {
+                sections[idx].classList.add('active');
+            }, 1100)
+        }
+    })
+})
+
+logoLinks.addEventListener('click', () => {
+    if (!navLinks[0].classList.contains('active')) {
+        activePage();
+        navLinks[0].classList.add('active');
+        setTimeout(() => {
+                sections[0].classList.add('active');
+            }, 1100)
+    }
+})
+
+const resumeBtns = document.querySelectorAll('.resume-btn');
  resumeBtns.forEach((btn, idx) => {
      btn.addEventListener('click', () => {
         const resumeDetails = document.querySelectorAll('.resume-detail');
@@ -7,4 +59,40 @@
          resumeBtns.forEach(b => b.classList.remove('active'));
          btn.classList.add('active');
      });
+ });
+ const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+ const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+ let index = 0;
+ const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carouse1 .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    })
+    portfolioDetails[index].classList.add('active');
+
+ }
+ arrowRight.addEventListener('click', () => {
+    if (index < 4){
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else{
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+    activePortfolio();
+ });
+ arrowLeft.addEventListener('click', () => {
+    if (index > 1){
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else{
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+    activePortfolio();
  });
